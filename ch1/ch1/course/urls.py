e6401bd2868caf4b55ccc17bed4a2558e0d3277a
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-01-17 12:32:28 +0530

    templates and dynamic templates

--- ch1/ch1/course/urls.py
+++ ch1/ch1/course/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from course.views import php_course,learn_django
+from course.views import php_course,learn_django,fast_api
 
 urlpatterns = [
     path('php/',php_course),
-    path('learn-django',learn_django)
+    path('learn-django',learn_django),
+    path('fast_api',fast_api)
 ]
--- ch1/ch1/course/views.py
+++ ch1/ch1/course/views.py
@@ -6,4 +6,13 @@ def php_course(request):
     return HttpResponse(f'<h1>Learn PHP Step by Step</h1>')
 
 def learn_django(request):
-    return HttpResponse('<h1>Hello Django from course App</h1>')
+    # return render(req,template_name,context=dic_name,content_type=mime_type,status=none,using=none)
+    seats = 10
+    coursename = {
+        'cname':'Django',
+        'st':seats
+    }
+    return render(request,'course/django.html',coursename)
+
+def fast_api(request):
+    return render(request,'course/fastapi.html')
