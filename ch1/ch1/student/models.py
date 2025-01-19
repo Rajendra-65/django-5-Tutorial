467a8dfe66a93d0fa51b172e3ffceade79e8b267
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-01-19 13:58:15 +0530

    learning the admin-site registration creating super user and returning the modified name in django

--- ch1/ch1/student/models.py
+++ ch1/ch1/student/models.py
@@ -7,3 +7,15 @@ class Profile(models.Model):
     roll = models.IntegerField()
     state = models.CharField(max_length = 70)
     comment = models.CharField(max_length=70,default='nothing')
+
+    def __str__(self):
+        # We can only provide the string data type.
+        return self.name
+
+class Result (models.Model):
+    stu_class = models.CharField(max_length = 70)
+    marks = models.IntegerField()
+
+    def __str__(self):
+        return self.stu_class
+    
