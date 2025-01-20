d7dc2cf996c4302f382694365d35e31a2617237a
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-01-20 11:44:47 +0530

    Building the table view in django admin panel

--- ch1/ch1/student/admin.py
+++ ch1/ch1/student/admin.py
@@ -3,5 +3,16 @@ from student.models import Profile,Result
 
 # Register your models here.
 
-admin.site.register(Profile)
-admin.site.register(Result)
+# admin.site.register(Profile)
+# admin.site.register(Result)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name','email','roll','city','state','comment')
+
+admin.site.register(Profile,ProfileAdmin)
+
+# @admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ('id','stu_class')
+
+admin.site.register(Result,ResultAdmin)
